7323ce5700dc2faba441da81badb74c950e5aadc
tarikjrh <tarikjarrah@hotmiail.com>
2023-06-09 16:58:52 +0300

    change

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,33 +1,34 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
-import './Page.css';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from "@ionic/react"
+import { useParams } from "react-router"
+import ExploreContainer from "../components/ExploreContainer"
+import "./Page.css"
 
 const Page: React.FC = () => {
+    const { name } = useParams<{ name: string }>()
 
-  const { name } = useParams<{ name: string; }>();
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>{name}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton />
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">{name}</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <h1>hiiii</h1>
 
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name={name} />
-      </IonContent>
-    </IonPage>
-  );
-};
+                <ExploreContainer name={name} />
+            </IonContent>
+        </IonPage>
+    )
+}
 
-export default Page;
+export default Page
