e083d2478cab5f31b4b41ac26b9bee4b6a69dcb0
tarikjrh <tarikjarrah@hotmiail.com>
2023-07-21 13:28:44 +0300

    sd

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import App from "./App"
 import { defineCustomElements } from "@ionic/pwa-elements/loader"
+import { getInitialContext } from "@ionic/portals"
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
 defineCustomElements(window)
+
+const context = getInitialContext()
+console.log(context)
 
 root.render(
     <React.StrictMode>
